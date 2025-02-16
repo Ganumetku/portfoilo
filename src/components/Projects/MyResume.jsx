b30bf7fe17135aa531a10e5a643ba4d5c460291b
Ganesh Metku <144722060+Ganumetku@users.noreply.github.com>
2025-02-16 21:24:58 +0530

    Update MyResume.jsx
    Changes  in Resumee

--- src/components/Projects/MyResume.jsx
+++ src/components/Projects/MyResume.jsx
@@ -3,7 +3,7 @@ import myresume from "../../../public/myresume2.png";
 const MyResume = () => {
   return (
     <div className='flex justify-center mt-20 mb-10'>
-      <img src={myresume} className=''></img>
+      <img src="https://drive.google.com/file/d/1sICWSeOETjzuGlQ4dkQ0CP6YKi8rs107/view?usp=sharing" className=''></img>
     </div>
   )
 }
