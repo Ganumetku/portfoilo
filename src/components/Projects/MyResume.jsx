6fbce7d0d3e23e74e24b1f58044b9a66253465b7
Ganesh Metku <144722060+Ganumetku@users.noreply.github.com>
2025-02-16 21:40:10 +0530

    Update MyResume.jsx
    Update

--- src/components/Projects/MyResume.jsx
+++ src/components/Projects/MyResume.jsx
@@ -1,15 +1,15 @@
 
-import React from 'react'
-import myresume from "../../../public/myresume2.png";
-const MyResume = () => {
-  return (
-    <div className='flex justify-center mt-20 mb-10'>
-       <a 
-        href="https://drive.google.com/file/d/1sICWSeOETjzuGlQ4dkQ0CP6YKi8rs107/view?usp=sharing" 
-        target="_blank" 
-        rel="noopener noreferrer" > </a>
-    </div>
-  )
-}
+// import React from 'react'
+// import myresume from "../../../public/myresume2.png";
+// const MyResume = () => {
+//   return (
+//     <div className='flex justify-center mt-20 mb-10'>
+//        <a 
+//         href="https://drive.google.com/file/d/1sICWSeOETjzuGlQ4dkQ0CP6YKi8rs107/view?usp=sharing" 
+//         target="_blank" 
+//         rel="noopener noreferrer" > </a>
+//     </div>
+//   )
+// }
 
-export default MyResume
+// export default MyResume
