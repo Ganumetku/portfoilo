18a4d44a93157e04e6df43661c9b5de72fcb10e9
Ganesh Metku <144722060+Ganumetku@users.noreply.github.com>
2025-02-17 09:14:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<HomeProject />} />
           {/* {/* <Route path="/about" element={<About />} /> */}
-          <Route path="/myresume" element={<MyResume />} />
+          
         </Routes>
         <Footer/>
         <Toaster/>
