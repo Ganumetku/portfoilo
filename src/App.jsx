8f808fd81a9baa7d1acdb7d67639a8d4b1be628b
Ganesh Metku <144722060+Ganumetku@users.noreply.github.com>
2025-02-17 09:02:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/HomePage/HomePage.jsx";
 import Footer from "./components/Footer";
 import HomeProject from "../src/components/Projects/HomeProject.jsx"
-import MyResume from "../src/components/Projects/MyResume.jsx"
+
 
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
